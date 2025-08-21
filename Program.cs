492256693f93abec6bb6f4c1ffab2093b3d460fb
Ahmed El-Hajjar <cdahmedeh@gmail.com>
2025-08-21 18:05:12 -0400

    My FSUIPC connection hello world!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using FSUIPC;
+
 Console.WriteLine("Welcome To LegacySimBridge!");
+
+try
+{
+    FSUIPCConnection.Open();
+}
+catch (FSUIPCException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+if (FSUIPCConnection.IsOpen)
+{
+    Console.WriteLine("Connected to FSUIPC!");
+}
